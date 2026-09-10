11925a9c14ff9df504e26097c48089e88a3462d9
VitorPimentel-git <vitor.pimenteel04@gmail.com>
2026-09-10 10:30:38 -0300

    Tela de perfil finalizada

--- lib/frontend/perfil_page.dart
+++ lib/frontend/perfil_page.dart
@@ -59,11 +59,27 @@ class _PerfilPage extends State<PerfilPage> {
     }
   }
 
-   void abrirEditarPerfil(BuildContext context) {
-    Navigator.push(
+  Future<void> abrirEditarPerfil(BuildContext context) async {
+    final biografia = await Navigator.push<String>(
       context,
-      MaterialPageRoute(builder: (context) => const EditarPerfilPage()),
+      MaterialPageRoute(
+        builder: (context) => EditarPerfilPage(
+          biografiaInicial: biografiaController.text,
+        ),
+      ),
     );
+
+    if (biografia != null && mounted) {
+      setState(() {
+        biografiaController.text = biografia;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    biografiaController.dispose();
+    super.dispose();
   }
 
   
@@ -71,7 +87,10 @@ class _PerfilPage extends State<PerfilPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF3E9DC),
-      body: Column(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+        
         children: [
           const SizedBox(
             height: 25,
@@ -175,10 +194,13 @@ class _PerfilPage extends State<PerfilPage> {
               
               TextField(
                 controller: biografiaController,
+                readOnly: true,
+                showCursor: false,
+                enableInteractiveSelection: false,
                 decoration: InputDecoration(
                   filled: true,
                   fillColor: const Color(0xFFD7CBBD),
-                  hintText: 'Digite sua biografia',
+                  hintText: 'Biografia',
                   prefixIcon: const Icon(
                     Icons.chat_bubble,
                     color: Color(0xFF5E3023),
@@ -200,7 +222,8 @@ class _PerfilPage extends State<PerfilPage> {
           ),
         ],
       ),
-    );
+    ),
+  );
   }
 }
 
